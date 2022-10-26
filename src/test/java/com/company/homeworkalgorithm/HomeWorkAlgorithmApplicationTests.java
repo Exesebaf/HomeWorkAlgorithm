af69c40bbcf160a63a26f5e135cb6c98c3989705
AleksandrVavilov <vavilov3388@gmail.com>
2022-10-26 12:49:31 +0300

    OneCommit

---
+++ src/test/java/com/company/homeworkalgorithm/HomeWorkAlgorithmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.homeworkalgorithm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeWorkAlgorithmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
